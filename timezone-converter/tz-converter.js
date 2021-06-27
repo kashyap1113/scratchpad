8885512c6634ab9bf4e5e8fdf0c88a7323f1a74c
Kashyap Kansara <kashyap.kansara@uffizio.in>
2021-06-27 23:30:39 +0530

    get timezone list from moment

--- timezone-converter/tz-converter.js
+++ timezone-converter/tz-converter.js
@@ -1,15 +1,46 @@
 var SQL_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
-$(document).on('blur paste', '#millis', function() {
-    console.log('input time added');
+$(document).ready(function() {
+    fillTimeZone('.output-timezone');
+});
+$(document).on('blur paste', '.millis', function(event) {
+    var inputTime = event.type == 'paste' 
+        ? event.originalEvent.clipboardData.getData('text/plain')
+        : $('.millis').val();
+    var tz = $('.output-timezone').val();
+    convertMillisToTimeZone(inputTime, tz);
 });
 
 
-$(document).on('change', '.output-timezone', function() {
-    console.log('timezone dropwon changed');
-    convertMillisToTimeZone($('#millis').val(), 'UTC+00:00');
+$(document).on('change', '.output-timezone', function(event) {
+    var tz = $('.output-timezone').val();
+    convertMillisToTimeZone($('.millis').val(), tz);
 });
 
 function convertMillisToTimeZone(millis, timeZone) {
+    if (millis == '')
+        return;
     var mmt = moment.tz(millis, 'x', timeZone); 
     console.log('converted time .....' + mmt.format(SQL_TIME_FORMAT));
+    var formatted = mmt.format(SQL_TIME_FORMAT)
+    $('.formatted').val(formatted);
+}
+
+var timeZoneList = [];
+function getTimeZoneList() {
+    timeZoneList = moment.tz.names();
+}
+
+function fillTimeZone(elementSelector) {
+    if (timeZoneList.length == 0)
+        getTimeZoneList();
+
+    var optionEl;
+    var selectEl = $(elementSelector);
+    for (var i = 0; i < timeZoneList.length; i++) {
+       optionEl = $('<option>', {
+           value : timeZoneList[i],
+           text : timeZoneList[i]
+       }); 
+       selectEl.append(optionEl);
+    }
 }
