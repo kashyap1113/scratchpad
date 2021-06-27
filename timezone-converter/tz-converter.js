e48d9b338989e83dfb5ca86ec9bbef1c1cbdad91
Kashyap Kansara <kashyap.kansara@uffizio.in>
2021-06-28 00:05:02 +0530

    added dropdown for converted timezone

--- timezone-converter/tz-converter.js
+++ timezone-converter/tz-converter.js
@@ -1,6 +1,7 @@
 var SQL_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 $(document).ready(function() {
     fillTimeZone('.output-timezone');
+    fillTimeZone('.to-timezone');
 });
 $(document).on('blur paste', '.millis', function(event) {
     var inputTime = event.type == 'paste' 
@@ -8,21 +9,34 @@ $(document).on('blur paste', '.millis', function(event) {
         : $('.millis').val();
     var tz = $('.output-timezone').val();
     convertMillisToTimeZone(inputTime, tz);
+    $('.to-timezone').trigger('change');
 });
 
 
 $(document).on('change', '.output-timezone', function(event) {
     var tz = $('.output-timezone').val();
     convertMillisToTimeZone($('.millis').val(), tz);
+    $('.to-timezone').trigger('change');
+});
+
+$(document).on('change', '.to-timezone', function(event) {
+    var tz = $('.to-timezone').val();
+    convertTimeZoneToMillis($('.formatted').val(), tz);
 });
 
 function convertMillisToTimeZone(millis, timeZone) {
     if (millis == '')
         return;
     var mmt = moment.tz(millis, 'x', timeZone); 
-    console.log('converted time .....' + mmt.format(SQL_TIME_FORMAT));
     var formatted = mmt.format(SQL_TIME_FORMAT)
     $('.formatted').val(formatted);
+}
+
+function convertTimeZoneToMillis(sqlTime, timeZone) {
+    if (sqlTime == '')
+        return;
+    var mmt = moment.tz(sqlTime, SQL_TIME_FORMAT, timeZone);
+    $('.converted-millis').val(mmt.format('x'));
 }
 
 var timeZoneList = [];
