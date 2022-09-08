b9e9faa17347c72d4abd7ee7c716031254ae1f9e
Kashyap Kansara <kashyap1113@gmail.com>
2022-09-08 18:41:31 +0530

    remove print statements

--- mac-battery-alert/battery-alert.sh
+++ mac-battery-alert/battery-alert.sh
@@ -3,8 +3,6 @@ batteryPercentage=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 powerSource=$(pmset -g batt | head -n 1 | cut -d \' -f2)
 batteryPower="Battery Power"
 acPower="AC Power"
-echo $batteryPercentage
-echo $powerSource
 if [ $batteryPercentage -lt 51 ] && [ "$powerSource" = "$batteryPower" ];
 then
 	osascript -e 'display dialog "Please, plug in the charger." buttons {"OK"} with icon caution'
