5ba52b3d206c4f2b9b2ec73bd098e75e003eaf33
kashyap-kansara-tp <kashyap.kansara@teampumpkin.com>
2025-01-29 20:21:17 +0530

    added nvim config

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,17 @@
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set hlsearch")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+vim.cmd("set spell")
+
+vim.cmd("filetype plugin indent on")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
